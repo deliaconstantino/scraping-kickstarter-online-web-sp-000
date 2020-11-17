b2e33abc0f71ca128112ce5675279ab1998962ef
Delia Constantino <dconstan92@gmail.com>
2020-11-17 20:16:39 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,7 @@ end
 #image linK: project.css("div.project-thumbnail a img").attribute("src").value
 #description: project.css("p.bbcard_blurb").text
 
-#
+#percent funded: project.css("ul.project-stats li.first.funded strong").text
 
 
 create_project_hash
